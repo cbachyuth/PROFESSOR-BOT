f857e8d5ae00120d23fc88c179f9e5c52b79c9ab
cbachyuth <132799400+cbachyuth@users.noreply.github.com>
2023-05-08 10:17:21 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -38,7 +38,7 @@ async def pm_next_page(bot, query):
         offset = 0
     search = PM_BUTTONS.get(key)
     if not search:
-        await query.answer("You are using one of my old messages, please send the request again.", show_alert=True)
+        await query.answer("You are using one of my old messages, please send the request again.", show_alert=False)
         return
 
     files, n_offset, total = await get_search_results(search, offset=offset, filter=True)
